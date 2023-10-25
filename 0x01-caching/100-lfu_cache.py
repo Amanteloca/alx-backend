213266d369be6a1fc100062dfb6f227bf75eb7b9
amanteloca <tonyanaky51@gmail.com>
2023-10-25 06:34:57 +0000

    update

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,50 +1,53 @@
-#!/usr/bin/python3
-""" Create class LFUCache, inherits from BaseCaching, a caching system """
-
-BaseCaching = __import__('base_caching').BaseCaching
+#!/usr/bin/env python3
+"""
+LFU caching module
+"""
+from base_caching import BaseCaching
+from typing import Any, Optional
 
 
 class LFUCache(BaseCaching):
-    """ class LFUCache """
-
+    """ LFU cache class
+    """
     def __init__(self):
-        """ Initialize class/function """
+        """ Initializes new instance
+        """
         super().__init__()
-        self.keys = []
-        self.uses = {}
+        self.counter = {}
 
-    def put(self, key, item):
-        """ function put """
-        if key is not None and item is not None:
-            if (len(self.keys) == BaseCaching.MAX_ITEMS
-                    and key not in self.keys):
-                discard = self.keys.pop(self.keys.index(self.findLFU()))
-                del self.cache_data[discard]
-                del self.uses[discard]
-                print('DISCARD: {:s}'.format(discard))
-            self.cache_data[key] = item
-            if key not in self.keys:
-                self.keys.append(key)
-                self.uses[key] = 0
-            else:
-                self.keys.append(self.keys.pop(self.keys.index(key)))
-                self.uses[key] += 1
+    def put(self, key: Any, item: Any) -> None:
+        """ Adds data to cache based on LRU policy
+            - Args:
+                - key: new entry's key
+                - item: entry's value
+        """
+        if not key or not item:
+            return
+        counter = self.counter
+        new_cache_data = {key: item}
+        old_cache_data = self.cache_data.get(key)
+        if len(self.cache_data) == self.MAX_ITEMS and not old_cache_data:
+            key_to_remove = list(counter.keys())[0]
+            self.cache_data.pop(key_to_remove)
+            counter.pop(key_to_remove)
+            print(f'DISCARD: {key_to_remove}')
+        self.cache_data.update(new_cache_data)
+        counter.update({key: counter.get(key, 0) + 1})
+        counter = dict(sorted(counter.items(),
+                              key=lambda x: (x[1], x[0])))
 
-    def get(self, key):
-        """ function get """
-        if key is not None and key in self.cache_data:
-            self.keys.append(self.keys.pop(self.keys.index(key)))
-            self.uses[key] += 1
-            return self.cache_data[key]
-        return None
-
-    def findLRU(self):
-        """ function findLRU """
-        items = list(self.uses.items())
-        freqz = [item[1] for item in items]
-        least = min(freqz)
-
-        lfuz = [item[0] for item in items if item[1] == least]
-        for key in self.keys:
-            if key in lfuz:
-                return key
+    def get(self, key: Any) -> Optional[Any]:
+        """ Gets cache data associated with given key
+            and updates dict in accordance to LFU policy
+            - Args:
+                - key to look for
+            - Return:
+                - value associated with the key
+        """
+        cache_item = self.cache_data.get(key)
+        counter = self.counter
+        if cache_item:
+            counter.update({key: counter.get(key) + 1})
+            counter = dict(sorted(counter.items(),
+                                  key=lambda x: (x[1], x[0])))
+        return cache_item
